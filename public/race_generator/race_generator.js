347a31deddb6bc21d8c3153cb7ebd369415020b9
DomtronVox <Domtron.Vox@Gmail.com>
2016-05-21 23:43:29 -0400

    Added loading all the parts of a race using a query string. Oddly enough I haven't implemented saving yet.

--- public/race_generator/race_generator.js
+++ public/race_generator/race_generator.js
@@ -155,7 +155,9 @@ $(window).load(function() {
 
     
 
-    //Final part is to setup the Race Summery section 
+    //next part is to setup the Race Summery section 
+
+    var chosen_parts = [];
 
     //delete the section_onclcik
     var delete_onclick = function(){
@@ -174,15 +176,16 @@ $(window).load(function() {
             return;
         }
 
-        //build a string that identifies the part
+        //build a string that identifies the part and make note
         var part_text = $("#NameText").text()+" ("
         $.each($("#SelectionInfo .Selected"), function() {
             part_text += $(this).text() + ", ";
         });
-        part_text += ")";
+        part_text += part_text.substr(0, part_text.length-2)+")";
 
         //get region the part belongs to and add it to that region in the race summery
         var region = $("#RegionsText .Selected").attr("internal_id").replace("Add", "");
+
         $("#RaceSummery"+capitalizeEachWord(region)).append("<li>"+part_text+"</li>");
         $("#RaceSummery"+capitalizeEachWord(region)+" li").click(delete_onclick);
 
@@ -194,13 +197,102 @@ $(window).load(function() {
         var side_to_add = "#racePartBuildLeft"
 
         //everything in the upper half of the list gets added to the right hand column
-        if(i > Object.keys(race_bio_regions).length/2-1){ side_to_add = "#racePartBuildRight"; }
-        if(region == "regionless") { continue; }        
+        if(i > Object.keys(race_bio_regions).length/2){ side_to_add = "#racePartBuildRight"; }
 
         $(side_to_add).append("<h4>"+capitalizeEachWord(region)+"</h4>")
                       .append("<ul id=\"RaceSummery"+capitalizeEachWord(region)+"\"></ul>")
         
         //increment count used to determin column
         i++;
+    }
+
+    //Parse query string
+    //Modified from: http://stackoverflow.com/a/13896633
+    function parseRaceFromQuery(str) {
+
+        if(typeof str != "string" || str.length == 0) return {};
+
+        var s = str.split("&");
+        var bit, parts = [], cur_part = {}, part_attr;
+
+        for(var i = 0; i < s.length; i++) {
+
+            bit = s[i].split("=");
+            name = decodeURIComponent(bit[0]);
+
+            if(name.length == 0) continue;
+
+            part_attr = decodeURIComponent(bit[1]);
+
+            part_attr = part_attr.split(",");
+
+            //setup part attrabutes
+            cur_part["name"] = name;
+            for (var pair in part_attr) {
+                pair = part_attr[pair].split(":");
+                cur_part[pair[0]] = pair[1];
+            }
+   
+            //add part to return value and reset current part for the next loop
+            parts.push(cur_part);
+            cur_part = {};
+        }
+
+        return parts;
+    }
+
+    //Builds a query string
+    function buildSaveRaceToQuery(params) {
+
+        var full_str = "", part_str = "";
+        for (part in params) {
+            
+            //part name is used as the query string's key
+            part_str = part["name"]+"=";
+            //go through each part of info that needs saving
+            for (key in Object.keys(part)) {
+                if (key == "name") { continue; } //skip name entry
+                
+                if (typeof part[key] == Array) {
+                    part_str = key + ":" + part[key].join(",") + ";";
+                } else {
+                    part_str = key + ":" + part[key] + ";";
+                }
+            }
+            
+            //add part to the full string
+            full_str += part_str + "&"
+        }
+        //return all but the last character (which is an extra &
+        //TODO maybe escape the , and : and ;
+        return full_str.substring(0,full_str.length-1);
+    }
+
+    //see if there is a query string with race info.
+    var loaded_race = parseRaceFromQuery( document.location.search.substring(1) )
+
+    //there is one. load it.
+    if (loaded_race.length != 0) {
+        var part_text = ""; 
+        for (var part in loaded_race) {
+            part = loaded_race[part];
+
+            //build part string
+            part_text = capitalizeEachWord(part["name"]) + " (";
+
+            for (var attr in part) {
+                if (attr == "name") { continue; }
+                part_text += capitalizeEachWord(part[attr]) + ", ";
+            }
+
+            //cut out extra comma and add ending parenthesis
+            part_text = part_text.substr(0, part_text.length-2)+")";
+
+            //add part to the race summery window
+            region = part["region"] || "regionless";
+
+            $("#RaceSummery"+capitalizeEachWord(region)).append("<li>"+part_text+"</li>");
+            $("#RaceSummery"+capitalizeEachWord(region)+" li").click(delete_onclick);
+        }
     }
 });
